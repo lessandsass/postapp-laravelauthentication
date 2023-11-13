a69bafe2b41769316e0df78022892c2a53455aad
lessandsass <lessandsass@gmail.com>
2023-11-13 06:06:56 +0530

    authorize user to access the dashboard page.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,11 @@ use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index() :View {
         return view('dashboard');
     }
