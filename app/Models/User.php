be79bc0b2cb55e78b75eb7f5b33bb566b3c9f88e
lessandsass <lessandsass@gmail.com>
2023-11-15 22:35:07 +0530

    post migration created, fillable properities was added in the Post and user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,23 +19,19 @@ class User extends Authenticatable
         'username'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
