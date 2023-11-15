4f0a4a90f69d1f2543a5994263835e0696a886ab
lessandsass <lessandsass@gmail.com>
2023-11-15 22:11:43 +0530

    post view was done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,6 +48,26 @@
                 </div>
 
                 <div class="mb-4">
+                    <label for="username" class="sr-only">Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="Choose a username"
+                        class="bg-gray-800 border-gray-700 border-2 w-full p-4 rounded-lg outline-none @error('username')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('username') }}"
+                    >
+
+                    @error('username')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
                     <label for="password" class="sr-only">Password</label>
                     <input
                         type="password"
